944670a675627777ed22dae5837b1ed1a0d65f1a
brunathemoteo <brunathemoteo@gmail.com>
2024-10-20 00:47:47 -0300

    ajust routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import controllerDoctor from "./controllers/controller.doctor.js";
 
 const router = Router();
+
+router.get("/doctors", controllerDoctor.Listar);
 
 export default router;
