5006a035ca0058a037076f9d4a18eb846b47c494
brunathemoteo <brunathemoteo@gmail.com>
2024-10-21 01:27:01 -0300

    Ajust query function

--- src/database/db.js
+++ src/database/db.js
@@ -7,12 +7,11 @@ const config = {
 	server: process.env.DB_SERVER,
 	database: process.env.DB_DATABASE,
 	options: {
-		encrypt: process.env.DB_ENCRYPT === "true", // Converte a string 'true' para booleano
-		trustServerCertificate: true, // Pode ser útil para desenvolvimento local
+		encrypt: process.env.DB_ENCRYPT === "true", 
+		trustServerCertificate: true,
 	},
 };
 
-// Função para conectar ao banco de dados
 let pool;
 async function connectToDatabase() {
 	if (!pool) {
@@ -21,28 +20,29 @@ async function connectToDatabase() {
 			console.log("Conectado ao banco de dados");
 		} catch (error) {
 			console.error("Erro ao conectar ao banco de dados:", error);
-			throw error; // Re-lança o erro para tratamento posterior
+			throw error;
 		}
 	}
 	return pool;
 }
 
-// Função para executar queries
-function query(command, params = []) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const pool = await connectToDatabase(); // Garante que a conexão esteja estabelecida
-			const request = pool.request(); // Cria uma nova requisição
-			// Aqui você pode adicionar os parâmetros se necessário
-			params.forEach((param) => {
-				request.input(param.name, param.value); // Adicione parâmetros como necessário
-			});
-			const result = await request.query(command); // Executa a consulta
-			resolve(result);
-		} catch (error) {
-			reject(error);
-		}
-	});
+async function query(command, params = {}) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const pool = await connectToDatabase(); 
+            const request = pool.request(); 
+            
+            for (const key in params) {
+                request.input(key, params[key]); 
+            }
+
+            const result = await request.query(command); 
+            resolve(result);
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
+
 
 export default { connectToDatabase, query };
