cb8d62bc6419a4aa302277086ceb949ba2e12978
brunathemoteo <brunathemoteo@gmail.com>
2024-10-20 23:37:03 -0300

    Update doctors return to recordset

--- src/repositories/repository.doctor.js
+++ src/repositories/repository.doctor.js
@@ -3,7 +3,7 @@ import db from "../database/db.js";
 async function getDoctors() {
 	const sql = "SELECT * FROM doctors";
 	const doctors = await db.query(sql, []);
-	return doctors;
+	return doctors.recordset;
 }
 
 export default { getDoctors };
