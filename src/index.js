69419160b7ba1fbbdede7c94f10296d3552f6336
brunathemoteo <brunathemoteo@gmail.com>
2024-10-20 23:56:52 -0300

    Fix port and update query list doctors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(router);
 app.use(express.json());
 
-PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`servidor is running on port ${PORT}`);
--- src/repositories/repository.doctor.js
+++ src/repositories/repository.doctor.js
@@ -1,7 +1,7 @@
 import db from "../database/db.js";
 
 async function getDoctors() {
-	const sql = "SELECT * FROM doctors";
+	const sql = "SELECT * FROM doctors ORDER BY name";
 	const doctors = await db.query(sql, []);
 	return doctors.recordset;
 }
