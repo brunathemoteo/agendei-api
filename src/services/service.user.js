a924d33cd89d5d49061c9f1f64519c6484ef413a
brunathemoteo <brunathemoteo@gmail.com>
2024-10-24 02:24:24 -0300

    Add bcrypt for user  password

--- src/services/service.user.js
+++ src/services/service.user.js
@@ -1,7 +1,11 @@
 import repositoryUser from "../repositories/repository.user.js"
+import bcrypt from "bcrypt";
 
 async function createUser (name, email, password) {
-    const user = await repositoryUser.createUser(name, email, password);
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await repositoryUser.createUser(name, email, hashPassword);
     return user;
 }
 
