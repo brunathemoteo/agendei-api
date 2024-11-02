c255fd1b0ea03fc7bd864c59549478cc0060f8a3
brunathemoteo <brunathemoteo@gmail.com>
2024-11-01 23:22:59 -0300

    Add route appointment

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,19 @@ import jwt from "./token.js"
 
 const router = Router();
 
+// Doctors
 router.get("/doctors", jwt.validateToken, controllerDoctor.listDoctors);
 router.post("/doctors", jwt.validateToken, controllerDoctor.createDoctor);
 router.put("/doctors/:id_doctor", jwt.validateToken, controllerDoctor.updateDoctor);
 router.delete("/doctors/:id_doctor", jwt.validateToken, controllerDoctor.deleteDoctor);
 router.get("/doctors/:id_doctor/services", jwt.validateToken, controllerDoctor.listDoctorsServices);
 
+// Users
 router.post("/user/register", controllerUser.createUser);
 router.post("/users/login", controllerUser.loginUser);
+
+// Appointments
+router.get("/appointments", jwt.validateToken, controllerAppointment.listAppointments)
+
 
 export default router;
