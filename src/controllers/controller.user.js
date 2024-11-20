4150589b989cb5d69d980c513a0ccae07a3ddcce
brunathemoteo <brunathemoteo@gmail.com>
2024-11-20 00:49:39 -0300

    fix: ajust user verification

--- src/controllers/controller.user.js
+++ src/controllers/controller.user.js
@@ -1,45 +1,42 @@
-import serviceAppointment from "../services/service.appointment.js";
-import serviceUser from "../services/service.user.js"
+import serviceAppointment from '../services/service.appointment.js';
+import serviceUser from '../services/service.user.js';
 
-async function createUser(req, res){
+async function createUser(req, res) {
+	const { name, email, password } = req.body;
 
-    const {name, email, password} = req.body;
+	if (!name || !email || !password) {
+		return res.status(400).json({ message: 'All fields are required.' });
+	}
 
-    if (!name || !email || !password){
-        return res.status(400).json({message: "All fields are required."});
-    }
-
-    try {
-        const user = await serviceUser.createUser(name, email, password);
-        if(!user){
-            return res.status(400).json({message: "User could not be created."});
-        }
-        res.status(201).json(user);
-        
-    } catch (error) {
-        console.log("Erro ao criar usuario: ", error);
-        return res.status(500).json({ message: "An error occurred while creating the user." });
-    }
-
+	try {
+		const user = await serviceUser.createUser(name, email, password);
+		if (!user) {
+			return res.status(400).json({ message: 'User could not be created.' });
+		}
+		res.status(201).json(user);
+	} catch (error) {
+		console.log('Erro ao criar usuario: ', error);
+		return res.status(500).json({ message: 'An error occurred while creating the user.' });
+	}
 }
 
 async function loginUser(req, res) {
-    const {email, password} = req.body;
+	const { email, password } = req.body;
 
-    const user = await serviceUser.loginUser(email, password);
+	const user = await serviceUser.loginUser(email, password);
 
-    if (user.length == 0){
-        return res.status(401).json({error: "Email ou senha inválida"});
-    }
+	if (!user || user.error) {
+		return res.status(401).json({ error: user ? user.error : 'Email ou senha inválidos.' });
+	}
 
-    return res.status(200).json(user)
+	return res.status(200).json(user);
 }
 
 async function profile(req, res) {
-    const id_user = req.id_user;
-    const user = await serviceUser.profile(id_user);
+	const id_user = req.id_user;
+	const user = await serviceUser.profile(id_user);
 
-    res.status(200).json(user);
+	res.status(200).json(user);
 }
 
-export default {createUser, loginUser, profile };
+export default { createUser, loginUser, profile };
